9fc1c00a88e6bdaa376b3095c1b54205f17310d1
intelburn <nerdacs@gmail.com>
2022-11-16 19:34:17 -0500

    Started to work on data model

--- dj_bpewad/main/models.py
+++ dj_bpewad/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class doctor(models.Model):
+    firstName = models.CharField(max_length=30)
+    lastName = models.CharField(max_length=30)
+    phone = models.CharField(max_length=10)
+
+class patient(models.Model):
+    firstName = models.CharField(max_length=30)
+    lastName = models.CharField(max_length=30)
+    address = models.CharField(max_length=60)
+    phone = models.CharField(max_length=10)
+    allergies = models.CharField(max_length=60)
+    doctor = models.ForeignKey(doctor, on_delete=models.CASCADE)
